2c72fb2994ca57037962fb8c827cf8ba5f89bc5b
Sergey Fomenko <fomenko.personal@gmail.com>
2021-09-25 17:39:07 +0300

    feat: add card likes methods

--- controllers/cards.js
+++ controllers/cards.js
@@ -20,3 +20,19 @@ module.exports.deleteCard = (req, res) => {
     .then((card) => res.send({ data: card }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
+
+module.exports.likeCard = (req, res) => {
+  const { cardId } = req.params;
+
+  Card.findByIdAndUpdate(cardId, { $addToSet: { likes: req.user._id } }, { new: true })
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.dislikeCard = (req, res) => {
+  const { cardId } = req.params;
+
+  Card.findByIdAndUpdate(cardId, { $pull: { likes: req.user._id } }, { new: true })
+    .then((card) => res.send({ data: card }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
